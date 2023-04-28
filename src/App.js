e9634c6e96fd371627717322c4eb1f7fe9e4c2c1
Juho Ahonen <juho.ahonen2@gmail.com>
2023-04-28 12:45:14 +0300

    foot, responsive

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="container">
-      <h1>ME</h1>
-      <a href='https://github.com/juhoohuj'>GITHUB</a>
+    <div className="App">
+      <div className="container">
+        <h1>ME</h1>
+        <a href='https://github.com/juhoohuj'>GITHUB</a>
+      </div>
+        <div className='footer'>
+        <p>© 2023 Juho Ahonen</p>
+      </div>
     </div>
+
   );
 }
 
